65da0fa42b2cc783cf180dacf661c7f6521eff25
Yannz2zWorld <yannzworld@gmail.com>
2026-07-03 11:47:24 +0700

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,15 @@
+module.exports = {
+  creatorName: "Yannz2z",
+  apiTitle: "Yannz2z Api",
+  webName: "Yannz Api",
+  favicon: "https://img2.pixhost.to/images/9050/745481347_yannganteng-1783009788991.jpg",
+  logoIcon: "⚡",
+  logoIconUrl: "https://img2.pixhost.to/images/9050/745481347_yannganteng-1783009788991.jpg",
+  whatsappLink: "https://wa.me/6281249703469",
+  githubLink: "https://github.com/Yannz2z",
+  instagramLink: "https://www.instagram.com/yannznms10?igsh=OXRlc2g1MHIydGky",
+  telegramLink: "https://t.me/Yannz2z",
+  whatsappChannelLink: "https://whatsapp.com/channel/0029VbCBj0N3LdQTosP3Hj0T",
+  emailAddress: "yannzworld@gmail.com",
+  apiKeys: ["yansuperiors"]
+};
