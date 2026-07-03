08d2f0fc3bd3d0039f25479fa40a9be1477170e7
Yannz2zWorld <yannzworld@gmail.com>
2026-07-03 11:30:45 +0700

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,15 @@
+module.exports = {
+  creatorName: "Alip Clutch",
+  apiTitle: "Clutch Api Hub",
+  webName: "Clutch Api",
+  favicon: "https://cdn-alip.clutch.web.id/api/u/mr1qezqk.jpg",
+  logoIcon: "⚡",
+  logoIconUrl: "https://cdn-alip.clutch.web.id/api/u/mr1qezqk.jpg",
+  whatsappLink: "https://wa.me/6281249703469",
+  githubLink: "https://github.com/alifalfrl",
+  instagramLink: "https://www.instagram.com/alifalfrl__?igsh=OG1jbjdubnIwOWtl",
+  telegramLink: "https://t.me/bigedikeman",
+  whatsappChannelLink: "https://whatsapp.com/channel/0029VbC3jusK5cDD9mELOb18",
+  emailAddress: "alifalfarel10@gmail.com",
+  apiKeys: ["alipclutch"]
+};
